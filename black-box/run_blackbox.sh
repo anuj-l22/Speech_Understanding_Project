8d2590a34282c4c568718a6eecc04a5b7e3bf736
Anuj Lalla <149088702+anuj-l22@users.noreply.github.com>
2025-04-17 10:49:04 +0530

    Add files via upload

--- black-box/run_blackbox.sh
+++ black-box/run_blackbox.sh
@@ -1,9 +1,11 @@
-# run HSJA signal, example for audioseal
-python3 HSJA_signal_audiomarkdata.py --gpu 0 --testset_size 200  --query_budget 10000  --tau 0.15 --norm linf --model audioseal --blackbox_folder audioseal_10k 
+#  python3 square_audiomarkdata.py --gpu 3 --testset_size 200  --query_budget 10  --tau 0.15 --model audioseal --blackbox_folder square10kbefore --attack_type both --eps 0.05 
 
-# run HSJA spectrogram, exmaple for audioseal 
-python3 HSJA_spectrogram_audiomarkdata.py --gpu 0 --testset_size 200  --query_budget 10000  --tau 0.15 --norm linf --model audioseal --blackbox_folder audioseal_10k --attack_type both
+#  python3 square_audiomarkdata.py --gpu 3 --testset_size 200  --query_budget 10  --tau 0.15 --model audioseal --blackbox_folder square10kafter --attack_type both --eps 0.05 --def_ok True 
 
-# run square attack, example for audioseal
-python3 square_audiomarkdata.py --gpu 0 --testset_size 200  --query_budget 10000  --tau 0.15 --model audioseal --blackbox_folder audioseal_10k --attack_type both --eps 0.05
+# python3 alif_attack.py --gpu 3 --testset_size 200 --query_budget 10 --tau 0.15 --model audioseal --blackbox_folder alif_10kbefore --attack_type both --eps 0.05 
+
+# python3 alif_attack.py --gpu 3 --testset_size 200 --query_budget 10 --tau 0.15 --model audioseal --blackbox_folder alif_10kafter --attack_type both --eps 0.05  --def_ok True 
+
+
+
 
