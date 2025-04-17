59dd52c0772ee2ea09c434329350bfe4d5d2e315
Anuj Lalla <149088702+anuj-l22@users.noreply.github.com>
2025-04-17 10:50:37 +0530

    Update run_blackbox.sh

--- black-box/run_blackbox.sh
+++ black-box/run_blackbox.sh
@@ -4,7 +4,7 @@
 
 # python3 alif_attack.py --gpu 3 --testset_size 200 --query_budget 10 --tau 0.15 --model audioseal --blackbox_folder alif_10kbefore --attack_type both --eps 0.05 
 
-# python3 alif_attack.py --gpu 3 --testset_size 200 --query_budget 10 --tau 0.15 --model audioseal --blackbox_folder alif_10kafter --attack_type both --eps 0.05  --def_ok True 
+python3 alif_attack.py --gpu 3 --testset_size 200 --query_budget 10 --tau 0.15 --model audioseal --blackbox_folder alif_10kafter --attack_type both --eps 0.05  --def_ok True 
 
 
 
